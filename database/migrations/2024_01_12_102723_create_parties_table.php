61ad7b700258981678ee61d7689c8066273166ce
Alaa Basim <alaabasim39@gmail.com>
2024-01-12 14:02:27 +0300

    Models + Migrations

---
+++ database/migrations/2024_01_12_102723_create_parties_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parties', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('video_id');
+            $table->stirng('name');
+            $table->string('url');
+            $table->datetime('start_time');
+            $table->datetime('end_time')->nullable();
+            $table->enum('status', ['live', 'ended'])->default('scheduled');
+            $table->timestamps();
+
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('video_id')->references('id')->on('videos')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parties');
+    }
+};
